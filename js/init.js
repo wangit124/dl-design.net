589ea058133f8bc691ef0c1da3ef99c1e9f4743f
howard wang <luhao.wang2016@gmail.com>
2019-04-23 17:04:59 -0700

    Styled text

--- js/init.js
+++ js/init.js
@@ -1,17 +1,3 @@
-/*(function($) {
- $(function(){
-  var introductionEl = $('div.introduction'),
-      introductionElOffset = introductionEl.offset().top/2,
-      documentEl = $(document);
-   
-  documentEl.on('scroll', function() {
-      if (documentEl.scrollTop() > introductionElOffset && introductionEl.hasClass('hidden) ) {
- 	introductionEl.removeClass('hidden');
-  });
-  });
-})();
-*/
-
 $(document).ready(function(){
   $('.materialboxed').materialbox();
 });
